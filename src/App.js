f917243d08a643de30a3014f7f4144f8d2d3ab31
Prachi More <prachimore21@navgurukul.org>
2022-12-18 20:31:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+function App(){
+  return(
+    <div id="box">
+      <table>
+        <th>Table</th>
+        <tr><td>prachi</td></tr>
+        <tr><td>jyoti</td></tr>
+        <tr><td>simran</td></tr>
+        <tr><td>saloni</td></tr>
+        <tr><td>sheetal</td></tr>
+        <tr><td>aarti</td></tr>
+        <tr><td>ishita</td></tr>
+        <tr><td>ashwini</td></tr>
+        <tr><td>shivani</td></tr>
+        <tr><td>deepshikha</td></tr>
+        <tr><td>anusha</td></tr>
+        <tr><td>trupti</td></tr>
+        <tr><td>aishani</td></tr>
+        <tr><td>manori</td></tr>
+        <tr><td>subhadra</td></tr>
+        <tr><td>sukanya</td></tr>
+      </table>
+    </div>
+  )
+}
+export default App;
